b25311b15ee03896d0ca5366e8d67c1e1ab5fa20
Avery <avery.j.sims@gmail.com>
2023-09-03 00:24:52 -0500

    Create Book.cs

---
+++ LibraryCatalogSystem/LibraryCatalogSystem/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCatalogSystem
+{
+	enum BookStatus
+	{
+		Available,
+		CheckedOut
+	}
+
+	internal class Book
+	{
+		// Properties
+		public int ISBN { get; set; }
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public BookStatus Status { get; set; }
+
+		// Constructor
+		public Book(int isbn, string title, string author, BookStatus status)
+		{
+			ISBN = isbn;
+			Title = title;
+			Author = author;
+			Status = status;
+		}
+	}
+}
