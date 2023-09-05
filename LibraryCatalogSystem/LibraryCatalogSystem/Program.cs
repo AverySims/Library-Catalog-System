08eb8aca7cc41263d80f6735fc14d3f4135cd620
AverySims <avery.j.sims@gmail.com>
2023-09-05 10:34:34 -0500

    Updated selection loop in program file, added more search functions to SearchManager class

--- LibraryCatalogSystem/LibraryCatalogSystem/Program.cs
+++ LibraryCatalogSystem/LibraryCatalogSystem/Program.cs
@@ -1,16 +1,101 @@
 ﻿using CustomConsole;
+using GenericParse;
 
 namespace LibraryCatalogSystem
 {
 	internal class Program
 	{
 		public static Dictionary<ulong, Book> Catalog { get; private set; } = Book.PresetLibrary;
+
+		private static string[] menu1 = { "View all books", "Search by ISBN", "Search by Title", "Search by Author", "Check out a book", "Return a book" };
+		private static string[] menu2 = { "Exit program" };
+
+		private static bool _loopMain = true;
+		
 		static void Main(string[] args)
 		{
+			while (_loopMain)
+			{
+				PrintMenu();
+				SelectMenuOption();
+			}
+		}
 
-			SearchManager.SearchByTitle("The", Catalog, out Dictionary<ulong, Book> results);
+		/// <summary>
+		/// Displays all menu options in the console.
+		/// </summary>
+		static void PrintMenu()
+		{
+			Console.WriteLine("Welcome to the smallest Library Catalog");
+			ConsoleHelper.PrintBlank();
+			ConsoleHelper.PrintStrings( new[] {menu1, menu2} );
+		}
 
-			ConsoleHelper.UserEndProgram();
+		/// <summary>
+		/// Waits for user input and calls SwitchOnMenuSelection(), passing the user's input as a parameter.
+		/// </summary>
+		private static void SelectMenuOption()
+		{
+			// looping until a valid option is selected
+			while (true)
+			{
+				ConsoleHelper.PrintBlank();
+				Console.Write("Select option: ");
+				int tempSelect = GenericReadLine.TryReadLine<int>();
+
+				if (!SwitchOnMenuSelection(tempSelect))
+				{
+					break;
+				}
+			}
+		}
+		
+		/// <summary>
+		/// Uses a switch statement to call the appropriate method based on the user's menu selection.
+		/// </summary>
+		/// <param name="selection">The user's menu selection</param>
+		/// <returns>The desired loop state</returns>
+		private static bool SwitchOnMenuSelection(int selection)
+		{
+			bool tempReturnValue = true;
+
+			// clearing console and printing menu again to prevent clutter
+			Console.Clear();
+			PrintMenu();
+			ConsoleHelper.PrintBlank();
+
+			switch (selection)
+			{
+				case 1: // View all books
+					SearchManager.PrintBooks(Catalog);
+					break;
+				case 2: // Search by ISBN
+					Console.Write("Search for a book by ISBN: ");
+					SearchManager.SearchByISBN(GenericReadLine.TryReadLine<ulong>(), Catalog, out Dictionary<ulong, Book> resultsISBN);
+					break;
+				case 3: // Search by Title
+					Console.Write("Search for a book via title: ");
+					SearchManager.SearchByTitle(GenericReadLine.TryReadLine<string>(), Catalog, out Dictionary<ulong, Book> resultsTitle);
+					break;
+				case 4: // Search by Author
+                    Console.Write("Search for a book via author: ");
+                    SearchManager.SearchByAuthor(GenericReadLine.TryReadLine<string>(), Catalog, out Dictionary<ulong, Book> resultsAuthor);
+					break;
+				case 5: // Check out book
+					
+					break;
+				case 6: // return book
+					
+					break;
+				case 7: // exit program
+					tempReturnValue = false;
+					_loopMain = false;
+					break;
+				default: // Invalid selection
+					ConsoleHelper.PrintInvalidSelection();
+					break;
+			}
+			return tempReturnValue;
 		}
 	}
 }
--- LibraryCatalogSystem/LibraryCatalogSystem/SearchManager.cs
+++ LibraryCatalogSystem/LibraryCatalogSystem/SearchManager.cs
@@ -7,11 +7,9 @@ public static class SearchManager
 {
 	public static void PrintBooks(Dictionary<ulong, Book> books)
 	{
-		ConsoleHelper.PrintBlank();
-
 		if (books.Count < 1)
 		{
-			Console.WriteLine("Couldn't find any books with that title.");
+			Console.WriteLine("Couldn't find any books with that parameter.");
 		}
 		else
 		{
@@ -19,9 +17,19 @@ public static class SearchManager
 			foreach (var kvp in books)
 			{
 				var book = kvp.Value;
-				Console.WriteLine($"Title: {book.Title}, Author: {book.Author}, Status: {book.Status}, ISBN: {kvp.Key}");
+				Console.WriteLine("------------------------------------------------------------------------------------------");
+				Console.WriteLine($"{book.Title} by {book.Author} | Status: {book.Status}, ISBN: {kvp.Key}");
 			}
 		}
+	}
+	
+	public static void SearchByISBN(ulong isbn, Dictionary<ulong, Book> catalog, out Dictionary<ulong, Book> results)
+	{
+		// using LINQ to filter results
+		results = catalog.Where(kvp => kvp.Key == isbn)
+			.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+		PrintBooks(results);
 	}
 	
 	public static void SearchByTitle(string title, Dictionary<ulong, Book> catalog, out Dictionary<ulong, Book> results)
@@ -29,20 +37,15 @@ public static class SearchManager
 		// using LINQ to filter results
 		results = catalog.Where(kvp => kvp.Value.Title.ToLower().Contains(title.ToLower()))
 			.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
-		
-		/* old method
-		 * 
-		Book[] tempList = catalog.Values.ToArray();
 
-		for (int i = 0; i < tempList.Length; i++)
-		{
-			// checking if the title matches (making both lowercase to avoid case sensitivity)
-			if (tempList[i].Title.ToLower().Contains(title.ToLower()))
-			{
-				results.Add(catalog.Keys.ToArray()[i], tempList[i]);
-			}
-		}
-		 */
+		PrintBooks(results);
+	}
+	
+	public static void SearchByAuthor(string author, Dictionary<ulong, Book> catalog, out Dictionary<ulong, Book> results)
+	{
+		// using LINQ to filter results
+		results = catalog.Where(kvp => kvp.Value.Author.ToLower().Contains(author.ToLower()))
+			.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
 
 		PrintBooks(results);
 	}
